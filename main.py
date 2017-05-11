2a4d5af698eaa7dfdd1ff3e4af7408ae20952be1
Steven Smith <nullspace000@gmail.com>
2017-05-11 14:29:54 -0700

    Implement twitter api search. Hash tag frequency table.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import json
+import pprint
 
 with open('creds.json') as data_file:    
     data = json.load(data_file)
@@ -13,7 +14,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+ 
+search_text = raw_input("Enter a hashtag #")
+tag_freq_table = {};
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+for tweet in tweepy.Cursor(api.search,
+                           q=search_text,
+                           count=10,
+                           result_type="recent",
+                           include_entities=True,
+                           lang="en").items():
+  tags = tweet.entities['hashtags'];
+  for tag_obj in tags:
+    tag = tag_obj['text'].lower();
+    if tag in tag_freq_table:
+      tag_freq_table[tag] += 1;
+    else:
+      tag_freq_table[tag] = 1;
+
+for tag in tag_freq_table:
+  freq = tag_freq_table[tag];
+  print(tag + " : " + str(freq));
